9c3805153f6db2806bd303077dfc7b11a62cc31b
Andrew Mestas <andrew.mestas.dev@gmail.com>
2016-03-09 07:40:38 -0800

    yelp functions in controller

--- controllers/api.js
+++ controllers/api.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var Yelp = require("yelp");
 var router  = express.Router();
 
 
@@ -31,9 +32,22 @@ router.get("/route", function(req, res) {
 // 3. Andrew will pass longitude and latitude to here and I will create route on google map
 
 router.get("/yelp", function(req, res) {
-	//var yelpData = yelp.search({type: 'food', location: 'Seattle'});
-	//res.send(yelpData);
-	res.send("testing");
+  var yelp = new Yelp({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    token: process.env.TOKEN,
+    token_secret: process.env.TOKEN_SECRET,
+  });
+
+	yelp.search({type: 'food', location: 'Seattle'})
+	.then(function (data) {
+	res.send(data);
+    console.log(data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+	// res.send("testing");
 });
 
 module.exports = router;
